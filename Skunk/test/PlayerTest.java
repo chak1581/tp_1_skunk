86923938155434c9bd0690ee9ba08f1c193da626
chak1581 <31973844+chak1581@users.noreply.github.com>
2018-03-03 21:11:06 -0600

    Update PlayerTest.java

--- Skunk/test/PlayerTest.java
+++ Skunk/test/PlayerTest.java
@@ -4,12 +4,6 @@ import org.junit.Test;
 
 public class PlayerTest {
 
-	@Test
-	public void testToString() {
-		
-		Player	player = new Player("Test1", 0, 50);
-		assertEquals("Player Name: " + "Test1" + " , Total Score: " + "0" + " , Total Chip: " + "50", player.toString());
-		
-	}
+	
 
 }
